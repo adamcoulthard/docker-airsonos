cd551c433e64c7ce17ebeee1720d5fed3f00cf0e
Adam Coulthard <adam@Adams-MacBook-Pro.local>
2018-08-30 21:32:48 +0100

    Change to setup_10 script.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 #rm /usr/bin/chfn
 #ln -s -f /bin/true /usr/bin/chfn
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
 
 apt-get -q update && apt-get install -qy \
   build-essential \
